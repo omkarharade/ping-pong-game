7fbd2885877fef24e8423881a0ec47e6bee87777
Omkar Harade <omkarharade1301@gmail.com>
2025-03-12 12:59:02 +0530

    added motion for paddle

--- index.js
+++ index.js
@@ -2,25 +2,65 @@ const paddle = document.getElementById("paddle");
 const ball  = document.getElementById("ball");
 const table = document.getElementById("ping-pong-table");
 
-let ball_X = ball.offsetTop;
-let ball_Y = ball.offsetHeight;
+let ball_X = ball.offsetLeft;
+let ball_Y = ball.offsetTop;
+let paddle_Y = paddle.offsetTop;
+
+
 
 const tableHeight = table.offsetHeight;
 const tableWidth = table.offsetWidth;
+
+const paddleHeight = paddle.offsetHeight;
+
+const ballHeight = ball.offsetHeight;
+const ballWidth = ball.offsetWidth;
 
 let dx = 5;   // pixel displacement per motion 
 let dy = 5;   // pixel displacement per motion 
 
 
-setInterval(function (){
 
-    if( ball_X <= 0 || ball_X > tableWidth) dx = -1 * dx;
-    if( ball_Y <= 0 || ball_Y > tableHeight) dy = -1 * dy;
 
-    ball_X += dx;
-    ball_Y += dy;
+document.addEventListener("DOMContentLoaded", (event) => {
 
-    ball.style.top = `${ball_Y + dy}px`
-    ball.style.left = `${ball_X + dx}px`
+    setInterval(function (){
 
-}, 100)
+        if( ball_X <= 0 || ball_X + ballWidth + dx > tableWidth) dx = -1 * dx;
+        if( ball_Y <= 0 || ball_Y + ballHeight + dy > tableHeight) dy = -1 * dy;
+
+        ball_X += dx;
+        ball_Y += dy;
+
+        ball.style.top = `${ball_Y}px`
+        ball.style.left = `${ball_X}px`
+
+    }, 40)
+
+
+    document.addEventListener("keydown", function (event){
+
+        const paddle_dy = 10;
+
+
+        if(event.key === "ArrowUp"){
+
+            // check : paddle should not go outside the table at top
+            if(paddle_Y - paddle_dy < 0) return;
+
+            // move the paddle up
+            paddle_Y -= paddle_dy;
+            paddle.style.top = `${paddle_Y}px`
+
+
+        }
+        else if(event.key === "ArrowDown"){
+
+            // check : paddle should not go outside the table at bottom
+            if(paddle_Y + paddle_dy + paddleHeight > tableHeight) return;
+
+            paddle_Y += paddle_dy;
+            paddle.style.top = `${paddle_Y}px`
+        }
+    })
+});
